18af819411264e6231ba2fd9ad49020058294eb1
Paul Frazee <pfrazee@gmail.com>
2017-04-30 15:46:38 -0500

    Add parseQS param

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const SCHEME_REGEX = /[a-z]+:\/\//i
 //                   1          2      3        4
 const VERSION_REGEX = /(dat:\/\/)?([^/]+)(@[^/]+)(.*)/i
 
-module.exports = function parseDatURL (str) {
+module.exports = function parseDatURL (str, parseQS) {
   // prepend the scheme if it's missing
   if (!SCHEME_REGEX.test(str)) {
     str = 'dat://' + str
@@ -13,10 +13,10 @@ module.exports = function parseDatURL (str) {
   var parsed, version = null, match = VERSION_REGEX.exec(str)
   if (match) {
     // run typical parse with version segment removed
-    parsed = parse((match[1] || '') + (match[2] || '') + (match[4] || ''))
+    parsed = parse((match[1] || '') + (match[2] || '') + (match[4] || ''), parseQS)
     version = match[3].slice(1)
   } else {
-    parsed = parse(str)
+    parsed = parse(str, parseQS)
   }
   parsed.href = str // overwrite href to include actual original
   parsed.version = version // add version segment
